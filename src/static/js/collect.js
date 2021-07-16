f4634d692d709289a63d2cfd3b8304e7b8ae8963
FKK1017 <danteeee0511@gmail.com>
2021-07-16 15:48:46 +0800

    bug fix

--- src/static/js/collect.js
+++ src/static/js/collect.js
@@ -17,7 +17,7 @@ function modify() {
 
     var obj = document.getElementsByClassName("post_some_context");
     for (var i = 0; i < obj.length; i++) {
-        obj[i].style.display="none";
+        obj[i].style.display = "none";
     }
 
     var obj = document.getElementsByClassName("post");
@@ -25,7 +25,7 @@ function modify() {
         obj[i].style.setProperty('width', '85%', 'important');
     }
 
-    setTimeout(function () {
+    setTimeout(function() {
         var obj = document.getElementsByClassName("delete");
         for (var i = 0; i < obj.length; i++) {
             obj[i].style.display = "flex";
@@ -47,11 +47,11 @@ function save() {
     setTimeout(function() {
         var obj = document.getElementsByClassName("post_some_context");
         for (var i = 0; i < obj.length; i++) {
-            obj[i].style.setProperty('display','','important');
+            obj[i].style.setProperty('display', '', 'important');
         }
-    },750);
+    }, 750);
 
-    
+
     var obj = document.getElementsByClassName("post");
     for (var i = 0; i < obj.length; i++) {
         obj[i].style.setProperty('width', '95%', 'important');
@@ -61,7 +61,7 @@ function save() {
     for (var i = 0; i < obj.length; i++) {
         obj[i].style.display = "none";
     }
-    setTimeout(function () {
+    setTimeout(function() {
         var obj = document.getElementsByClassName("cancel");
         for (var i = 0; i < obj.length; i++) {
             obj[i].style.setProperty('background-color', 'white', 'important')
@@ -70,16 +70,16 @@ function save() {
 }
 
 function del(p) {
-    var n=window.name;
-    $.ajax(url + '/ucollectpost',{
-        async:true,
-        type:"POST",
-        contentType:"application/json",
-        data:JSON.stringify({
-        n,
-        p,
+    var n = window.name;
+    $.ajax(url + '/ucollectpost', {
+        async: true,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            n,
+            p,
         }),
-    }).done(function(data){
+    }).done(function(data) {
         if (data) {
             //删除
             var de = document.getElementById(p);
@@ -91,15 +91,15 @@ function del(p) {
 }
 
 function get_collect() {
-    var n=window.name;
-    $.ajax(url + '/getusecollect',{
-        async:true,
-        type:"POST",
-        contentType:"application/json",
-        data:JSON.stringify({
-        n,
+    var n = window.name;
+    $.ajax(url + '/getusecollect', {
+        async: true,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({
+            n,
         }),
-    }).done(function(data){
+    }).done(function(data) {
         if (data == false) {
             var l = document.getElementById("mycollect_li");
             l.innerHTML = '<div class="empty">这里什么都没有找到T^T</div>';
@@ -107,10 +107,13 @@ function get_collect() {
             var l = document.getElementById("mycollect_li");
             l.innerHTML = "";
             for (var i = 0; i < data.length; i++) {
-                l.innerHTML = '<div class="post" id="' + data[i]['post_id'] + '" onclick="read_topic(' + data[i]['post_id'] + ')"><div class="post_title">'
-                + data[i]['title'] + '</div><div class="post_some_context">'
-                + data[i]['content'] + '</div><div class="post_info"><div id="thumb1" style="float: right;">点赞 200</div><div id="comment1" style="float: right;">评论 100</div><div id="share1" style="float: right;">转发 100</div></div></div><div class="cancel" id="' + data[i]['post_id'] + '"><div id="' + data[i]['post_id'] + '" class="delete" onclick="del(' + data[i]['post_id'] + ')">删除</div></div>'
-                + l.innerHTML;
+                l.innerHTML = l.innerHTML + '<div class="post" id="' + data[i]['post_id'] + '" onclick="read_topic(' + data[i]['post_id'] + ')"><div class="post_title">' +
+                    data[i]['title'] + '</div><div class="post_some_context">' +
+                    data[i]['content'] + '</div>' +
+                    '<div class="post_info"><div id="thumb1" style="float: right;display: flex;">' + '<img id="zan" src="./images/b_Zan.png" alt="" class="homeLike">' + '<div class="num">' + data[i]['like_count'] + '</div>' + '</div>' +
+                    '<div id="comment1" style="float: right;display: flex;">' + '<img id="store" src="./images/b_Store1.png" alt="" class="homeStore">' + '<div class="num">' + data[i]['collect_count'] + '</div>' + '</div>' +
+                    '<div id="share1" style="float: right;display: flex;">' + '<img id="comment" src="./images/Comment.png" alt="" class="homeComment">' + '<div class="num">' + data[i]['comment_count'] + '</div></div></div></div>' +
+                    '<div class="cancel" id="' + data[i]['post_id'] + '"><div id="' + data[i]['post_id'] + '" class="delete" onclick="del(' + data[i]['post_id'] + ')">删除</div></div>'
             }
         }
     })
