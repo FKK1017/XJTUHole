8d6cd30c0fc0ce4a7f18c2777497d384a66cbfbd
FKK1017 <danteeee0511@gmail.com>
2021-07-20 00:30:02 +0800

    举报

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -1,7 +1,11 @@
-window.onload=function() {
+window.onload = function() {
     console.log("running")
     mui('#picture').on('tap', 'li>a', function() {
         mui.alert("你刚点击了\"" + this.innerHTML + "\"按钮");
-    //  mui("#picture").popover('toggle');//这是可以用来关闭底部弹窗的事件
+        //  mui("#picture").popover('toggle');//这是可以用来关闭底部弹窗的事件
+    })
+
+    mui('.opt').on('tap', 'a', function() {
+        console.log(this.parentNode.parentNode);
     })
 }
